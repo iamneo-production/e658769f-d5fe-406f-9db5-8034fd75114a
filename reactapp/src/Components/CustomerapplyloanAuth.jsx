92131949686bd741aab4c94298db54fc13041477
kavyabonthu <kavyabonthu2001@gmail.com>
2023-07-14 18:19:21 +0000

    commiting to main

--- reactapp/src/Components/CustomerapplyloanAuth.jsx
+++ reactapp/src/Components/CustomerapplyloanAuth.jsx
@@ -8,9 +8,7 @@ function CustomerapplyloanAuth(values) {
     const amount_pattern= /^\d*$/
     const repay_pattern= /^\d*$/
   
-    if (!values.applicantMobile) {
-        error.applicantMobile= "Mobile number can't be empty";
-    } else if (!mobilenumber_pattern.test(values.applicantMobile)) {
+    if (!mobilenumber_pattern.test(values.applicantMobile)) {
         error.applicantMobile = "Invalid mobile number format";
     } else {
         error.applicantMobile="";
@@ -22,25 +20,19 @@ function CustomerapplyloanAuth(values) {
         error.applicantName="";
     }
   
-    if(!values.applicantEmail) {
-        error.applicantEmail="Email can't be empty";
-    } else if(!email_pattern.test(values.applicantEmail)) {
+    if(!email_pattern.test(values.applicantEmail)) {
         error.applicantEmail="Invalid email format";
     } else {
         error.applicantEmail="";
     }
   
-    if(!values.applicantAadhaar) {
-        error.applicantAadhaar="Aadhar number can't be empty";
-    } else if(!aadhar_pattern.test(values.applicantAadhaar)) {
+    if(!aadhar_pattern.test(values.applicantAadhaar)) {
         error.applicantAadhaar="Invalid aadhar number format";
     } else {
         error.applicantAadhaar="";
     }
   
-    if(!values.applicantPan) {
-        error.applicantPan="PAN number can't be empty";
-    } else if(!pan_pattern.test(values.applicantPan)) {
+    if(!pan_pattern.test(values.applicantPan)) {
         error.applicantPan="Invalid PAN number format";
     } else {
         error.applicantPan="";
@@ -52,27 +44,21 @@ function CustomerapplyloanAuth(values) {
         error.applicantAddress="";
     }
   
-    if (!values.applicantSalary) {
-        error.applicantSalary="Salary can't be empty";
-    } else if(!salary_pattern.test(values.applicantSalary)) {
+    if(!salary_pattern.test(values.applicantSalary)) {
         error.applicantSalary="Invalid salary format";
     } 
     else {
         error.applicantSalary="";
     }
   
-    if (!values.loanAmountRequired) {
-        error.loanAmountRequired="Amount can't be empty";
-    } else if(!amount_pattern.test(values.loanAmountRequired)) {
+    if(!amount_pattern.test(values.loanAmountRequired)) {
         error.loanAmountRequired="Invalid amount format";
     }
     else {
         error.loanAmountRequired="";
     }
   
-    if (!values.loanRepaymentMonths) {
-        error.loanRepaymentMonths="Number of months can't be empty";
-    } else if(!repay_pattern.test(values.loanRepaymentMonths)) {
+    if(!repay_pattern.test(values.loanRepaymentMonths)) {
         error.loanRepaymentMonths="Invalid month format";
     }
     else {
