fe505cbe04cb9dcb4c6323417c2476d77c1dad94
Puligadda-Pavan <pavanpuligadda123@gmail.com>
2023-07-29 15:50:30 +0000

    initial commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
